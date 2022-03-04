4161c2032c4aed7e7d61d8cb24af90f1c39d53e3
AniAntonyan <94914782+AniAntonyan@users.noreply.github.com>
2022-03-04 07:30:01 +0400

    first commit

---
+++ .history/index_20220227172243.js
@@ -0,0 +1,5 @@
+let input = document.getElementById("num");
+let btn = document.getElementById("btn");
+let text = document.getElementById("show-text");
+let restart = document.getElementById("restart");
+let random = Math.floor(Math.random() * 6 + 1);
---
+++ .history/index_20220227173422.js
@@ -0,0 +1,24 @@
+let input = document.getElementById("num");
+let btn = document.getElementById("btn");
+let text = document.getElementById("show-text");
+let restart = document.getElementById("restart");
+let random = Math.floor(Math.random() * 6 + 1);
+
+function result(){
+ 
+    return new Promise((resolve,reject)=>{
+        if(isNaN(+input.value) || (+input.value > 6 )|| (+input.value < 1)){
+            reject("it is error");
+        }
+    else if(random === +input.value){
+        resolve("2 points.correct number ");
+    }
+    else if((random-1 === +input.value) || (random+1 === +input.value)){
+        resolve("1 points.correct number is "+ random);
+       }else{
+       resolve("0 points.correct number is " + random);
+    }
+    
+    })
+    
+    }
